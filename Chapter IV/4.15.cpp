4e126ba18ec8685d94750342577aa4db8980ae87
sunlike <1136866770@qq.com>
2023-04-08 17:51:34 +0800

    test

--- Chapter IV/4.15.cpp
+++ Chapter IV/4.15.cpp
@@ -10,5 +10,5 @@ int main()
     cout << "cups value = " << cups;
     cout << " and cups address = " << &cups << endl;
     return 0;
-
+    
 }
---
+++ Chapter IV/4.16.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+int main()
+{
+    using namespace std;
+    int updates = 6;
+    int * p_updates;
+    p_updates = & updates;
+
+    cout << "Value: updates = " << updates;
+    cout << ", * p_updates = " << *p_updates << endl;
+
+    cout << "Adreess: &updates = " << &updates;
+    cout << ", p_updates = " << p_updates << endl;
+
+    *p_updates = *p_updates + 1;
+    cout << "Now updates = " << updates << endl;
+    return 0;
+}
