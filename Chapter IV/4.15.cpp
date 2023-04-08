a76dc805371f4c73be0f2e881432b6fff850aa54
sunlike <1136866770@qq.com>
2023-04-08 17:42:32 +0800

    email

--- Chapter IV/4.15.cpp
+++ Chapter IV/4.15.cpp
@@ -2,13 +2,13 @@
 int main()
 {
     using namespace std;
-    int dounts = 6;
+    int donuts = 6;
     double cups = 4.5;
 
-    cout << "dounts value = " << dounts;
-    cout << " and donuts address = " << &dounts << endl;
+    cout << "dounts value = " << donuts;
+    cout << " and donuts address = " << &donuts << endl;
     cout << "cups value = " << cups;
     cout << " and cups address = " << &cups << endl;
     return 0;
-    
+
 }
