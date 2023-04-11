73148ff74757bbe3db49025d88f665b09509114b
wiserking <1136866770@qq.com>
2023-04-11 18:49:07 +0800

    five commit

---
+++ Chapter V/1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+int main()
+{
+    using namespace std;
+    int i;
+    for(i = 0; i < 5; i++)
+    {
+        cout << "C++ knows loops" << endl;
+
+    }
+    cout << "C++ knows when to stop" << endl;
+    return 0;
+}
